8ab70d7c76b95b9cb23d5f7054e4d68a9a24955a
Alexander Pakhotin <turrbor@gmail.com>
2019-04-12 20:14:13 +0500

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,36 +1,45 @@
+BGGREEN='\033[42m'     #  ${BGGREEN}
 # УСТАНОВКА ТВИК УТИЛИТЫ:
-echo "Ставим утилиту для подстройки Gnome"
+echo -en "${BGGREEN} Ставим утилиту для подстройки Gnome"
+sleep 5
 sudo apt install -y gnome-tweak-tool
 
 # ВЕРНУТЬ ДОПОЛНЕНИЯ ДЛЯ GNOME:
-echo "Включаем расширения для Gnome"
+echo -en "${BGGREEN} Включаем расширения для Gnome"
+sleep 5
 sudo apt install -y chrome-gnome-shell
 
 # ТЕМЫ ИКОНОК NUMIX
-echo "Добавляем репозиторий с темами и устанавливаем тему и значки Numix"
+echo -en "${BGGREEN} Добавляем репозиторий с темами и устанавливаем тему и значки Numix"
+sleep 5
 sudo add-apt-repository -y ppa:numix/ppa
 sudo apt install -y numix-gtk-theme numix-icon-theme-circle
 
 # ПОДДЕРЖКА 32-БИТ БИБЛИОТЕК
-echo "Включаем поддержку 32-битных приложений и ставим необходимый минимум"
+echo -en "${BGGREEN} Включаем поддержку 32-битных приложений и ставим необходимый минимум"
+sleep 5
 sudo dpkg --add-architecture i386
 sudo apt install -y libc6:i386 libasound2:i386 libasound2-data:i386 libasound2-plugins:i386 libgtk2.0-0:i386 libsdl2-2.0-0:i386 libsdl2-image-2.0-0:i386 libfreetype6:i386 libcurl3:i386
 
 # КОДЕКИ, ДОПОЛНЕНИЯ
-echo "Устанавливаем кодеки, шрифты и прочие надобности"
+echo -en "${BGGREEN} Устанавливаем кодеки, шрифты и прочие надобности"
+sleep 5
 sudo apt install -y ubuntu-restricted-extras libavcodec-extra browser-plugin-freshplayer-pepperflash build-essential libgtk2.0-dev cmake gparted mono* aspell aspell-ru p7zip-rar p7zip-full &&
 
 # АРХИВАТОРЫ
-echo "Устанавливаем набор различных архиваторов"
+echo -en "${BGGREEN} Устанавливаем набор различных архиваторов"
+sleep 5
 sudo apt install -y p7zip-rar rar unrar unace arj cabextract
 
 # ПАКЕТ JDK, JRE И ПЛАГИН ДЛЯ БРАУЗЕРА
-echo "Устанавливаем Java для системы и браузера"
+echo -en "${BGGREEN} Устанавливаем Java для системы и браузера"
+sleep 5
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt install -y oracle-java8-installer
 
 # ОБНОВЛЕНИЕ МИКРОКОДА INTEL
-echo "Обновляем микрокод процессора"
+echo -en "${BGGREEN} Обновляем микрокод процессора"
+sleep 5
 # sudo apt install --reinstall intel-microcode &&
 
 # ОБНОВЛЕНИЕ МИКРОКОДА AMD
@@ -47,10 +56,12 @@ sudo apt install --reinstall amd64-microcode
 # если строка не пуста, добавьте значения через пробел к остальным
 
 #NVIDIA NATIVE DRIVER
-echo "Добавляем репозиторий с проприетарными драйверами Nvidia"
+echo -en "${BGGREEN} Добавляем репозиторий с проприетарными драйверами Nvidia"
+sleep 5
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 
 #TMP ПАПКУ В ОПЕРАТИВНУЮ ПАМЯТЬ
-echo "Помещаем папку для временных файлов в оперативную память"
+echo -en "${BGGREEN} Помещаем папку для временных файлов в оперативную память"
+sleep 5
 echo 'tmpfs /tmp tmpfs noatime,nodiratime,mode=1777,size=50% 0 0' | sudo tee -a /etc/fstab
 #Здесь size=50% устанавливает максимальный объём диска в половину всей физической памяти (это также и значение по умолчанию). Изменения вступят в силу после перезагрузки.
